a5198de242ae16cbfbb912ca85229e2da050dbc3
takemoto <hahaha>
2016-10-08 01:38:31 +0900

    Hello again.

---
+++ Java8/src/HelloRealWorldAgain.java
@@ -0,0 +1,12 @@
+
+
+public class HelloRealWorldAgain {
+
+    public static void main(String[] args) {
+
+	// TODO Auto-generated method stub
+	System.out.println("I'm a human working as an engineer.");
+
+    }
+
+}
