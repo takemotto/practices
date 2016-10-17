d5821499601e5bf28a3075669257fba4e0fd1447
takemotto <takemotto2@yahoo.co.jp>
2016-10-16 22:35:03 -0700

    First commit from Debian via git.

---
+++ Java8/src/practice/basics/HelloWorldFromDebian.java
@@ -0,0 +1,12 @@
+package practice.basics;
+
+/**
+ * Created by takemotto on 10/16/16.
+ */
+public class HelloWorldFromDebian {
+    private static final String GREETING = "Hello World, I'm on Debian via git.";
+
+    public static void main(String[] args) {
+        System.out.print(GREETING);
+    }
+}
